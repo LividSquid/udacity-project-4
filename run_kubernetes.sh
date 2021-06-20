579211f48a42a0a29ab1afa654f7efa765b05bd8
Tyson Richardson <trich92tr@gmail.com>
2021-06-20 03:53:12 +0000

    upload to docker hub and run k8s

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
+# Added optional parameter to specify username
+username=${1:-lividsquid}
+echo Using Docker Hub user $username
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=${username}/project-4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment project-4 --image=${dockerpath}:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
+# Step 3.5:
+# Wait for pod to have a status of "Running"
+count=0
+while [ $count -le 12 ]; do
+    if [ $(kubectl get pods | grep -q "Running"; echo $?) -eq 0 ]; then break; fi
+    if [ $count -eq 12 ]; then echo "Pod not running after 60 seconds. Exiting."; exit 1; fi
+    printf "Pod is not running yet. Waiting 5 seconds"
+    tick=0
+    while [ $tick -lt 5 ]; do sleep 1; printf "."; tick=$((tick+1)); done
+    printf "\n"
+    count=$((count+1))
+done
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/project-4 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Added optional parameter to specify username
+username=${1:-lividsquid}
+echo Using Docker Hub user $username
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
@@ -6,10 +9,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=${username}/project-4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u ${username}
+docker image tag project-4:latest ${dockerpath}:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}:latest
